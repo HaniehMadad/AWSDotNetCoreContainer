7aae0d2a56a020c6f823fdb953adf938c1d5b8d0
Hanieh Madad <hanieh.madad@gmail.com>
2018-02-15 13:49:38 +1000

    aws scripts

--- aws/aws-create-env.sh
+++ aws/aws-create-env.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 APP_NAME="testHanieh"
-ENV_NAME="testDocker13"
-App_Version="testdocker6"
+ENV_NAME="testDocker14"
+App_Version="testdocker14"
 
 aws elasticbeanstalk check-dns-availability --cname-prefix $ENV_NAME
 aws elasticbeanstalk create-environment --application-name $APP_NAME --version-label $App_Version --environment-name $ENV_NAME --template-name dockerconfig --solution-stack-name "64bit Amazon Linux 2017.09 v2.8.4 running Multi-container Docker 17.09.1-ce (Generic)"
 
+eb create $ENV_NAME --version $App_Version --cfg dockerconfig
